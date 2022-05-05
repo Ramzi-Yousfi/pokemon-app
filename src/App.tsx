9ed8ebaecbd7d2981f97567a0c36d0cb3b7d90ba
ramzi <ramzi446@hotmail.com>
2022-05-05 20:14:58 +0200

    show the pokemon with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import React,{FunctionComponent , useState} from 'react';
+import React, {FunctionComponent, useState, useEffect} from 'react';
+import POKEMONS from "./models/mock-pokemon";
+import Pokemon from "./models/pokemon";
 
-const App : FunctionComponent = () => {
-  const [name, setName] = useState<Array<string|number>>(['ramzi',2]);
-  return (
-    <div>
-      {
-        name.map(he =><h1>{he}</h1>)
+let pokemon = POKEMONS
+type Props = {
+    name: string
+}
+const Hello: FunctionComponent<{ name: string }> = ({name}) => {
+    const [target, setTarget] = useState<boolean>(false)
+    const toggle = () => {
+        setTarget(!target)
+    }
+    console.log({})
+    return (<>
+            <button onClick={() => toggle()} className="btn btn-primary">afficher</button>
+            {target && <img src={name} alt=""/>}
+        </>
+    )
+}
 
-      }
 
-    </div>
-  );
-  }
+const App: FunctionComponent = () => {
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    useEffect(() => {
+        setPokemons(POKEMONS)
+    }, [])
+
+    return (
+        <>
+            <h2>{pokemons.length}</h2>
+            <div className="album py-5 bg-light">
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        {pokemons.map(pokemon =>
+                                <div className="card shadow-sm">
+                                    <div className="bd-placeholder-img card-img-top" >
+                                        <Hello name={pokemon.picture}/>
+                                    </div>
+                                    <div className="text-center">
+                                        <h1>{pokemon.name}</h1>
+                                    </div>
+
+                                </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
 export default App;
