f2321d9f0f44efa37c7cd7436aadfe57785328cc
ramzi <ramzi446@hotmail.com>
2022-05-05 15:04:05 +0200

    models of pokemon in a ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{FunctionComponent , useState} from 'react';
 
-function App() {
+const App : FunctionComponent = () => {
+  const [name, setName] = useState<Array<string|number>>(['ramzi',2]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        name.map(he =><h1>{he}</h1>)
+
+      }
+
     </div>
   );
-}
-
+  }
 export default App;
